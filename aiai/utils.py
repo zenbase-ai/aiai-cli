c3883f02378fe45ade634f65d49f873f6b1f37f8
Cyrus <1880797+CyrusNuevoDia@users.noreply.github.com>
2025-04-17 15:17:03 -0600

    Add reset_db util

--- aiai/utils.py
+++ aiai/utils.py
@@ -4,9 +4,17 @@ import django
 from django.core.management import call_command
 
 
+def reset_db():
+    from aiai.app.models import FunctionInfo, OtelSpan
+
+    FunctionInfo.objects.all().delete()
+    OtelSpan.objects.all().delete()
+
+
 def setup_django():
     # Point to your minimal_django settings
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aiai.app.settings")
     django.setup()
+
     # Run migrations silently
     call_command("migrate", verbosity=0, interactive=False)
